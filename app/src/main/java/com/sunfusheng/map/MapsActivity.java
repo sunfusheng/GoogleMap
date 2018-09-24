6dac8a5c705ec619a0e51955308cd85629412ee4
sunfusheng <sfsheng0322@gmail.com>
2018-09-24 21:51:48 +0800

    init

--- app/src/main/java/com/sunfusheng/map/MapsActivity.java
+++ app/src/main/java/com/sunfusheng/map/MapsActivity.java
@@ -2,8 +2,15 @@ package com.sunfusheng.map;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +20,9 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.sunfusheng.StickyHeaderDecoration;
+import com.sunfusheng.map.adapter.StickyGroupAdapter;
+import com.sunfusheng.map.utils.StatusBarUtil;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleMap.OnMarkerClickListener {
@@ -23,10 +33,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap googleMap;
 
+    private BottomSheetDialog bottomSheetDialog;
+    private BottomSheetBehavior bottomSheetBehavior;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        StatusBarUtil.setTranslucentForImageView(this, 60, null);
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -34,13 +48,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         initData();
+        initView();
     }
 
     private void initData() {
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.support_map_fragment);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+    }
+
+    private void initView() {
+        findViewById(R.id.btn_menu_more).setOnClickListener(v -> {
+            if (bottomSheetDialog == null) {
+                initBottomSheetDialog();
+            } else {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                bottomSheetDialog.show();
+            }
+        });
+    }
+
+    private void initBottomSheetDialog() {
+        View bottomSheetView = LayoutInflater.from(this).inflate(R.layout.layout_recycler_view, null, false);
+        RecyclerView recyclerView = bottomSheetView.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new StickyHeaderDecoration());
+        StickyGroupAdapter stickyAdapter = new StickyGroupAdapter(this, Constants.items);
+        recyclerView.setAdapter(stickyAdapter);
+
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetBehavior = BottomSheetBehavior.from((View) bottomSheetView.getParent());
+        int height = getWindowManager().getDefaultDisplay().getHeight();
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) bottomSheetView.getLayoutParams();
+        layoutParams.height = height / 2;
+        bottomSheetView.setLayoutParams(layoutParams);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        bottomSheetDialog.show();
+
+        bottomSheetDialog.setOnDismissListener(dialog -> {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        });
     }
 
     @Override
@@ -51,7 +100,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void initGoogleMap() {
         UiSettings uiSettings = googleMap.getUiSettings();
-        uiSettings.setZoomControlsEnabled(true);
 
         LatLng sydney = new LatLng(-34, 151);
         googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
---
+++ app/src/main/java/com/sunfusheng/map/adapter/StickyGroupAdapter.java
@@ -0,0 +1,53 @@
+package com.sunfusheng.map.adapter;
+
+import android.content.Context;
+
+import com.sunfusheng.GroupRecyclerViewAdapter;
+import com.sunfusheng.GroupViewHolder;
+import com.sunfusheng.map.R;
+
+/**
+ * @author sunfusheng on 2018/9/23.
+ */
+public class StickyGroupAdapter extends GroupRecyclerViewAdapter<String> {
+
+    public StickyGroupAdapter(Context context, String[][] groups) {
+        super(context, groups);
+    }
+
+    @Override
+    public boolean showFooter() {
+        return false;
+    }
+
+    @Override
+    public int getHeaderLayoutId(int viewType) {
+        return R.layout.item_group_header;
+    }
+
+    @Override
+    public int getChildLayoutId(int viewType) {
+        return R.layout.item_group_item;
+    }
+
+    @Override
+    public int getFooterLayoutId(int viewType) {
+        return 0;
+    }
+
+    @Override
+    public void onBindHeaderViewHolder(GroupViewHolder holder, String item, int groupPosition) {
+        holder.setText(R.id.text, item);
+    }
+
+    @Override
+    public void onBindChildViewHolder(GroupViewHolder holder, String item, int groupPosition, int childPosition) {
+        holder.setText(R.id.text, item);
+    }
+
+    @Override
+    public void onBindFooterViewHolder(GroupViewHolder holder, String item, int groupPosition) {
+
+    }
+
+}
